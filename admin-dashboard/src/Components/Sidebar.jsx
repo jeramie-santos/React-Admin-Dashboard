b2238d5f76b80d602da84e4c3da164f20f927c08
Jeramie <jeramieramsantos79@gmail.com>
2026-04-15 22:16:27 +0800

    fixed navbar to sticky in large screen and fixed on smaller screen

--- admin-dashboard/src/Components/Sidebar.jsx
+++ admin-dashboard/src/Components/Sidebar.jsx
@@ -1,13 +1,14 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = ({isOpen, setIsOpen}) => {
 
-    const open = "fixed w-70 z-50 h-full transition-transform duration-300 block";
-
     const navActive = "bg-(--color-primary-dark) ml-2 transition duration-300 p-2 rounded-lg"
+    
 
     return (
-        <nav  className={`bg-(--color-sidebar) text-white py-10 ${isOpen ? open : "hidden"} lg:flex lg:flex-1`}>
+        <nav  className={`bg-(--color-sidebar) text-white py-10 z-50
+            ${isOpen ? " h-screen w-60 fixed" : "hidden"}  
+            lg:flex lg:sticky lg:top-0 lg:h-screen`}>
             <div className="flex flex-col gap-4 p-10">
                 <h1 className="font-bold text-2xl">Admin Dashboard</h1>
                 <NavLink to="/" end className={({isActive}) => 
